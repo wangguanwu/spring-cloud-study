3ab7cc43e6c405f06481b8bf60a9c9de295f0935
wangguanwu <2531507093@qq.com>
2022-08-25 10:52:17 +0800

    [feat] first commit

---
+++ mall-order/src/main/java/com/tuling/mall/order/MallOrderApplication.java
@@ -0,0 +1,15 @@
+package com.tuling.mall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
